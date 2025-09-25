349fb0ed07d5252d650e0d88f35f64f11699f75f
lijas <lijason991@gmail.com>
2025-09-25 16:08:05 -0400

    added questions to comments

--- src/main/java/MathApp.java
+++ src/main/java/MathApp.java
@@ -3,7 +3,7 @@ import static java.lang.Math.sqrt;
 public class MathApp {
     public static void main(String[] args) {
 
-        // Question 1
+        // Question 1: find larger of two salaries
 
         double salaryBob = 35000;
         double salaryGary = 85000;
@@ -13,7 +13,7 @@ public class MathApp {
 
         System.out.println("The highest salary is $" + salaryHighest + ".");
 
-        // Question 2
+        // Question 2: find smaller of two prices
 
         double priceCar = 5000;
         double priceTruck = 10000;
@@ -21,20 +21,20 @@ public class MathApp {
 
         System.out.println("The lowest priced vehicle costs $" + priceLowest + ".");
 
-        // Question 3
+        // Question 3: find area of circle
 
         double radius = 1.5;
-        double pi = 3.14159265359;
+        double pi = 3.141592653589;
         double areaCircle = pi * Math.pow(radius, 2);
         System.out.println("The area of the circle is " + areaCircle + ".");
 
-        // Question 4
+        // Question 4: find square root of variable = 5.0
 
         double varSqrt = 5.0;
         double Sqrt = Math.sqrt(varSqrt);
         System.out.println("The square root of " + varSqrt + " is " + Sqrt + ".");
 
-        // Question 5
+        // Question 5: find distance between 2 points
 
         int[] point1 = {5, 10};
         int[] point2 = {85, 50};
@@ -45,18 +45,18 @@ public class MathApp {
         );
         System.out.println("The distance between the two points is " + distance + ".");
 
-        // Question 6
+        // Question 6: find absolute value of variable = -3.8
 
         double varAbs = -3.8;
         double Abs = Math.abs(varAbs);
         System.out.println("The absolute value is " + Abs + ".");
 
-        // Question 7
+        // Question 7: create variable with random value from 0-1
 
         double randNum = Math.random();
         System.out.println("Here is a random number between 0 and 1: " + randNum + ".");
 
-        // Question 8
+        // Question 8: find number of minutes and milliseconds in 24 days
 
         int numDays = 24;
         int hoursPerDay = 24;
